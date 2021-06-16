ad81743ccd4840d74b694f2cd411daf6abd7c2fa
ducminhcao <minhduc.itus@gmail.com>
2021-06-16 10:33:00 +0700

    feat: add user success

--- src/view/pages/UITestPage.js
+++ src/view/pages/UITestPage.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 
 import SideDrawer from '../../components/SideDrawer/SideDrawer';
 import CreateUser from '../../components/CreateUser/CreateUser';
+import AddUserSuccess from '../../components/AddUserSuccess/AddUserSuccess';
 
 const UITestPage = (props) => {
   return (
     <div className="w-2/3">
       <SideDrawer id="ngu">
-        <CreateUser></CreateUser>
+        <AddUserSuccess></AddUserSuccess>
       </SideDrawer>
     </div>
   );
