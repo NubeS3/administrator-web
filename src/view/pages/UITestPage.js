178b23e9b75c6d6406db4d1200b662540ed83c2e
Tri Nguyen <kingpro068@gmail.com>
2021-06-16 11:56:56 +0700

    update ban user ui

--- src/view/pages/UITestPage.js
+++ src/view/pages/UITestPage.js
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import SideDrawer from '../../components/SideDrawer/SideDrawer';
 import CreateUser from '../../components/CreateUser/CreateUser';
 import AddUserSuccess from '../../components/AddUserSuccess/AddUserSuccess';
+import BanUser from '../../components/DeleteUserSideDrawer/BanUser';
+import SuccessBanUser from '../../components/DeleteUserSideDrawer/SuccessBanUser';
 
 const UITestPage = (props) => {
   return (
     <div className="w-2/3">
-      <SideDrawer id="ngu">
-        <AddUserSuccess></AddUserSuccess>
+      <SideDrawer>
+        <BanUser></BanUser>
       </SideDrawer>
     </div>
   );
