16a3b4b042c36ad07eea04d846cfd12b35739586
ducminhcao <minhduc.itus@gmail.com>
2021-06-16 10:22:01 +0700

    feat: user create

--- src/view/pages/UITestPage.js
+++ src/view/pages/UITestPage.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
 import SideDrawer from '../../components/SideDrawer/SideDrawer';
+import CreateUser from '../../components/CreateUser/CreateUser';
 
 const UITestPage = (props) => {
   return (
-    <div>
+    <div className="w-2/3">
       <SideDrawer id="ngu">
-        <button onClick={SideDrawer.close}>abcs</button>
+        <CreateUser></CreateUser>
       </SideDrawer>
     </div>
   );
